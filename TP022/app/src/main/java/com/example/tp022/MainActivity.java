dd16bad14cd443b1eed2c45f8402184518cbcc7f
wael <waelxvv@gmail.com>
2020-11-09 22:46:51 +0100

    TP2.2

---
+++ TP022/app/src/main/java/com/example/tp022/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.tp022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+public class MainActivity extends AppCompatActivity {
+    TextView mshowCount;
+    EditText editText;
+    int mCount=0;
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mshowCount.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mshowCount.getText().toString());
+        }
+        if (editText.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", editText.getText().toString());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mshowCount= findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        if (savedInstanceState != null) {
+            boolean isVisible =
+                    savedInstanceState.getBoolean("reply_visible");
+
+            if (isVisible) {
+                mshowCount.setVisibility(View.VISIBLE);
+                mshowCount.setText(savedInstanceState.getString("reply_text"));
+                mshowCount.setVisibility(View.VISIBLE);
+
+                editText.setVisibility(View.VISIBLE);
+                editText.setText(savedInstanceState.getString("reply_text"));
+                editText.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    public void countButton(View view) {
+        mCount++;
+        if (mshowCount !=null)
+            mshowCount.setText(Integer.toString(mCount));
+    }
+}
